a7e06b1f6935ce99e631e327a312b8952cdb2c97
Annah Muthoni <annahmuthoni93@gmail.com>
2023-06-15 15:44:44 -0700

    c- Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - prints "programming is like building a multilingual
  * puzzle, followed by a newline.
- * Rreturn: Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -11,6 +11,6 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
 
