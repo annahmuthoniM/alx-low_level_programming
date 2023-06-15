888ed3fed22228ad1b0795de6fdcbb44963a8e9f
Annah Muthoni <annahmuthoni93@gmail.com>
2023-06-15 13:04:38 -0700

    C- Hello World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<studio.h>
+
+/**
+ * main.Entry point
+ *
+ * Return:Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char.%d bytes(s)\n",sizeof(char));
+	printf("size of an int:%d bytes(s)\n",sizeof(int));
+	printf("size of long int:%d bytes(s)\n,sizeof(long int));
+	printf("size of long long int:%d bytes(s)\n,sizeof(long long int));
+	printf("size of float:%d bytes(s)\n,sizeof(float));
+	return(0);
+}
+	
