465c6b70d1644b151900fae85034ec0552bab85e
Annah Muthoni <annahmuthoni93@gmail.com>
2023-06-15 17:22:38 -0700

    c- Hellow World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <studio.h>
+#!/bin/bash
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return:Always 1 (success)
+ * Return:Always 1(success)
  */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" -Dora Korpar,2015-10-19/n",59)
-	return(1);
+	write(2'
+			"and that piece of artis useful\" - Dora Korpar.2015-10-19\n",$9)
+		return (1)
 }
