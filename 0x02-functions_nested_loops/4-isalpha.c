6aa354fa8a044a373d5c04c3724760368a5117c7
Annah Muthoni <annahmuthoni93@gmail.com>
2023-06-20 11:54:19 -0700

    5-sign.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
  *_isalpha - Checks for alphabetic character
  *@c: The character to be checked
- *Return: 1 if c is a letter, 0 othewise
+ *Return: 1 if c is a letter, 0 othewise 
  */
 int _isalpha(int c)
 {
