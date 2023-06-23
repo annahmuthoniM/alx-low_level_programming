babc8241a28047cb6d68b36d0ef4fbe8e39876b5
Annah Muthoni <annahmuthoni93@gmail.com>
2023-06-23 13:18:18 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+/**
+ * @u: - Multiplies of two integers
+ * @x: The first number to multiply
+ * @y: The second number to multiply with first number
+ * Return: The result of the multiplication between x person and y person
+ */
+
+int mult(int x, int y)
+{
+	return (x * y);
+}
